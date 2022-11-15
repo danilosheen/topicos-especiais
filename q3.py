cfec089dcc4497c65cc711e042b1e6f02853de65
danilosheen <c.danilo.f.silva@gmail.com>
2022-11-15 07:26:47 -0300

    q3

--- q3.py
+++ q3.py
@@ -2,3 +2,24 @@
 3. (fatores) Programa que lê um número inteiro positivo n e determina a sua decomposição 
 em fatores primos calculando também a multiplicidade de cada fator.
 '''
+
+def main():
+
+    n = int(input("Digite um numero (>1): "))
+
+    fator = 2 # primeiro fator
+    while n != 1:
+        # conta a multiplicidade de fator em n
+        mult = 0;
+        while n%fator == 0:
+            n = n / fator;
+            mult = mult + 1;
+
+        # imprime a multiplicade do fator
+        if mult != 0:
+            print("fator %d multiplicidade %d" %(fator, mult))
+
+        fator = fator + 1
+
+#-------------------------------------------------------
+main() # chamada da função principal
