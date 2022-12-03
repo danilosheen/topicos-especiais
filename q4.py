81313ea2e941fb4237fb9ae0185730b68e562407
danilosheen <c.danilo.f.silva@gmail.com>
2022-12-03 20:48:18 -0300

    4

--- q4.py
+++ q4.py
@@ -2,3 +2,18 @@
 4. (diofantina) Programa que lê três números inteiros positivos a, b, c, com mdc(a, b) =
 1 e determina a solução mínima x, y inteira para equação ax + by = c.
 '''
+
+# calcular mdc com teorema de euclides
+def mdc_euclides(a, b):
+
+    anterior  = a
+    atual     = b
+
+    resto = atual % anterior
+    while resto != 0:
+        resto = anterior % atual
+        anterior = atual
+        atual = resto
+
+    return anterior
+
