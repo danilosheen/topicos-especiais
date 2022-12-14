3e9a268924e9b18dcaa73fbc94e610f736e4a36c
danilosheen <c.danilo.f.silva@gmail.com>
2022-12-14 18:47:16 -0300

    atualização invertíveis

--- q8.py
+++ q8.py
@@ -4,19 +4,39 @@ seu Conjunto de Invertíveis Módulo n, ou seja, {b1, b2, · · · , bφ(n)} tal
 (mod n) implica i = j.
 '''
 
-def euclides(a, b):
-    if b == 0:
-        return a
-    else:
-        return euclides(b, a % b)
+# Importa a biblioteca math para calcular o valor de phi(n)
+import math
 
-def invertiveis(n):
-    invertiveis = []
-    for i in range(1, n):
-        if euclides(i, n) == 1:
-            invertiveis.append(i)
-    return invertiveis
+# Função para calcular o valor de phi(n)
+def phi(n):
+  # Inicializa o contador de coprimos
+  count = 0
 
+  # Percorre todos os inteiros i entre 1 e n
+  for i in range(1, n + 1):
+    # Verifica se i é coprimo com n
+    if math.gcd(i, n) == 1:
+      # Incrementa o contador de coprimos
+      count += 1
+
+  # Retorna o valor de phi(n)
+  return count
+
+# Solicita ao usuário que informe o valor de n
 n = int(input("Informe o valor de n: "))
-invertiveis = invertiveis(n)
-print(invertiveis)
+
+# Calcula o valor de phi(n)
+phi_n = phi(n)
+
+# Cria uma lista vazia para armazenar o conjunto de invertíveis módulo n
+invertiveis = []
+
+# Percorre todos os inteiros i entre 1 e n
+for i in range(1, n + 1):
+  # Verifica se i é coprimo com n
+  if math.gcd(i, n) == 1:
+    # Adiciona i à lista de invertíveis módulo n
+    invertiveis.append(i)
+
+# Exibe o conjunto de invertíveis módulo n para o usuário
+print("O conjunto de invertíveis módulo n é:", invertiveis)
